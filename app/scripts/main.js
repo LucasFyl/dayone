a1d0e6aada7d9e348bafbc149dcbc3625ad90c93
Lucas F <lfayolle1@gmail.com>
2015-05-07 17:22:41 +0200

    fixed gruntfile and jshint + test anim mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,8 @@
+
+
+
 $( document ).ready(function(){
+	// $('body').removeClass("scriptDisabled");
 	var isMobile = window.matchMedia('only screen and (max-width: 760px)');
 
     if (isMobile.matches) {
@@ -8,4 +12,11 @@ $( document ).ready(function(){
     } else {
         console.log('It\'s Not !');
     }
+
+    $('.landing h4, .landing h1, .slide#first .watch').addClass('from-bottom');
+	$('.landing p, .cta').addClass('fadeIn');
+
+    TweenMax.set('.from-bottom', {y:50});
+    TweenMax.staggerTo('.from-bottom', 0.5, {opacity:1,y:0,ease:Power3.easeOut,delay:0.5}, 0.1);
+    TweenMax.staggerTo('.fadeIn', 0.8, {opacity:1,ease:Power3.easeOut,delay:1.2});
 });

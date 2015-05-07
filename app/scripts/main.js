bd3c44846743477f26b9565c3dd269c5c0d0972a
Lucas F <lfayolle1@gmail.com>
2015-05-07 17:36:36 +0200

    arrow behaviour

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,10 +13,23 @@ $( document ).ready(function(){
         console.log('It\'s Not !');
     }
 
+    //  Landing part animation : 
     $('.landing h4, .landing h1, .slide#first .watch').addClass('from-bottom');
 	$('.landing p, .cta').addClass('fadeIn');
-
     TweenMax.set('.from-bottom', {y:50});
     TweenMax.staggerTo('.from-bottom', 0.5, {opacity:1,y:0,ease:Power3.easeOut,delay:0.5}, 0.1);
     TweenMax.staggerTo('.fadeIn', 0.8, {opacity:1,ease:Power3.easeOut,delay:1.2});
+
+    // arrow-link trigger first scroll : 
+    $('body').on('click', '.cta a', function(){
+    	$('html, body').animate({scrollTop: $('#first').offset().top}, 'slow');
+    	$('.arrow-down').css('opacity', 1);
+    });
+
+    // arrow down behaviour :
+    $('body').on('click', '.arrow-down', function(){
+    	var winH = $(window).height();
+    	$('html, body').animate({scrollTop: winH}, 'slow');
+    });
+
 });
